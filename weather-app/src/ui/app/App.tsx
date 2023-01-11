a2d36288227ea6fe4ca3cec623688d2ea7481634
benyamin parsa <benyaminparsa727@gmail.com>
2023-01-11 20:26:02 +0330

    [Fix] layout.

--- weather-app/src/ui/app/App.tsx
+++ weather-app/src/ui/app/App.tsx
@@ -1,17 +1,46 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import '../app/App.css'
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Header, Content, Footer } = Layout;
 
-  return (
-    <div className="App">
-        <div>
-            Main
-        </div>
-    </div>
-  )
-}
+const App: React.FC = () => {
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
 
-export default App
+    return (
+        <Layout>
+            <Header style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%' }}>
+                <div
+                    style={{
+                        float: 'left',
+                        width: 120,
+                        height: 31,
+                        margin: '16px 24px 16px 0',
+                        background: 'rgba(255, 255, 255, 0.2)',
+                    }}
+                />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    items={new Array(3).fill(null).map((_, index) => ({
+                        key: String(index + 1),
+                        label: `nav ${index + 1}`,
+                    }))}
+                />
+            </Header>
+            <Content className="site-layout" style={{ padding: '0 50px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <div style={{ padding: 24, minHeight: 380, background: colorBgContainer }}>Content</div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+        </Layout>
+    );
+};
+
+export default App;
--- weather-app/src/ui/app/components/Card.tsx
+++ weather-app/src/ui/app/components/Card.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import Axios from "axios";
 
 const Card = (props:any) => {
-    const fetchWeather = async (e) => {
-        e.preventDefault();
+    const fetchWeather = async () => {
         const response = await Axios.get(
             `https://api.openweathermap.org/data/2.5/weather?q=${'mashhad'}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
         );
         console.log(response,'test')
     };
-    fetchWeather('')
+    fetchWeather()
     return(
         <div>{props.family}</div>
     )
