97b71980f53f511355ce7fbe275b8be9c6601b40
benyamin parsa <benyaminparsa727@gmail.com>
2023-01-11 20:37:06 +0330

    [Fix] layout.

--- weather-app/src/ui/main.tsx
+++ weather-app/src/ui/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../ui/app/App'
-import Card from '../ui/app/components/Card'
-import CardClass from  '../ui/app/components/CardClass'
 import '../index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <Card family={'Parsa'}/>
-    <CardClass/>
   </React.StrictMode>
 )
